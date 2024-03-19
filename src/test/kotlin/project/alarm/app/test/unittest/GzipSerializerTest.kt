ca2d53af2c7644a4c82dcefe070b7513328f036d
devjun10 <devjun1023@gmail.com>
2024-03-19 17:01:41 +0900

    test: 깨진 테스트 복구 - AWS SNS/SQS 의존성을 추가함으로 깨진 테스트 복구

--- src/test/kotlin/project/alarm/app/test/unittest/GzipSerializerTest.kt
+++ src/test/kotlin/project/alarm/app/test/unittest/GzipSerializerTest.kt
@@ -1,7 +1,7 @@
 package project.alarm.app.test.unittest
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.kotest.core.spec.DisplayName
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import project.alarm.app.common.configuration.redis.GzipSerializer
 import project.alarm.app.logger
