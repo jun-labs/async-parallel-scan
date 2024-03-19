1314c3ac7721e6efbc4c7efd02a50f7e5786e6d6
devjun10 <devjun1023@gmail.com>
2024-03-19 17:22:39 +0900

    refactor: DeadLetter 설정 추가

--- src/main/kotlin/project/alarm/app/core/alarm/event/publisher/IndexDividerPublisher.kt
+++ src/main/kotlin/project/alarm/app/core/alarm/event/publisher/IndexDividerPublisher.kt
@@ -2,31 +2,44 @@ package project.alarm.app.core.alarm.event.publisher
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import project.alarm.app.core.alarm.event.IndexDivisionEvent
 import project.alarm.app.core.event.EventPublisher
+import software.amazon.awssdk.core.exception.SdkException
 import software.amazon.awssdk.services.sns.SnsClient
 import software.amazon.awssdk.services.sns.model.PublishRequest
 
 @Component
 class IndexDividerPublisher(
     @Value("\${application.amazon.sqs.event-divider.name}")
-    private val queueName: String,
+    private val queue: String,
 
     @Value("\${application.amazon.sqs.event-divider.topic}")
     private val topic: String,
+
+    @Value("\${application.amazon.sqs.event-divider.dead-letter-name}")
+    private val deadLetterQueue: String,
+
+    @Value("\${application.amazon.sqs.event-divider.dead-letter-topic}")
+    private val deadLetterTopic: String,
 
     private val snsClient: SnsClient,
     private val objectMapper: ObjectMapper
 ) : EventPublisher<IndexDivisionEvent> {
 
-    @Async("asyncThreadPool")
     override fun publishEvent(event: IndexDivisionEvent) {
-        val newEvent = PublishRequest.builder()
-            .topicArn("$topic:$queueName")
-            .message(objectMapper.writeValueAsString(event))
-            .build()
-        snsClient.publish(newEvent)
+        try {
+            val newEvent = PublishRequest.builder()
+                .topicArn("$topic:$queue")
+                .message(objectMapper.writeValueAsString(event))
+                .build()
+            snsClient.publish(newEvent)
+        } catch (ex: SdkException) {
+            val deadLetter = PublishRequest.builder()
+                .topicArn("$deadLetterTopic:$deadLetterQueue")
+                .message(objectMapper.writeValueAsString(event))
+                .build()
+            snsClient.publish(deadLetter)
+        }
     }
 }
--- src/main/kotlin/project/alarm/app/core/user/event/publisher/UserSearchEventPublisher.kt
+++ src/main/kotlin/project/alarm/app/core/user/event/publisher/UserSearchEventPublisher.kt
@@ -5,27 +5,41 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import project.alarm.app.core.event.EventPublisher
 import project.alarm.app.core.user.event.UserSearchEvent
+import software.amazon.awssdk.core.exception.SdkException
 import software.amazon.awssdk.services.sns.SnsClient
 import software.amazon.awssdk.services.sns.model.PublishRequest
 
 @Component
 class UserSearchEventPublisher(
     @Value("\${application.amazon.sqs.user-search.name}")
-    private val queueName: String,
+    private val queue: String,
 
     @Value("\${application.amazon.sqs.user-search.topic}")
     private val topic: String,
+
+    @Value("\${application.amazon.sqs.user-search.dead-letter-name}")
+    private val deadLetterQueue: String,
+
+    @Value("\${application.amazon.sqs.user-search.dead-letter-topic}")
+    private val deadLetterTopic: String,
 
     private val snsClient: SnsClient,
     private val objectMapper: ObjectMapper
 ) : EventPublisher<UserSearchEvent> {
 
     override fun publishEvent(event: UserSearchEvent) {
-        val eventAsString = objectMapper.writeValueAsString(event)
-        val newEvent = PublishRequest.builder()
-            .topicArn("$topic:$queueName")
-            .message(eventAsString)
-            .build()
-        snsClient.publish(newEvent)
+        try {
+            val newEvent = PublishRequest.builder()
+                .topicArn("$topic:$queue")
+                .message(objectMapper.writeValueAsString(event))
+                .build()
+            snsClient.publish(newEvent)
+        } catch (ex: SdkException) {
+            val deadLetter = PublishRequest.builder()
+                .topicArn("$deadLetterTopic:$deadLetterQueue")
+                .message(objectMapper.writeValueAsString(event))
+                .build()
+            snsClient.publish(deadLetter)
+        }
     }
 }
