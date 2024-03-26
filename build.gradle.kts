152fd9de9fc7db17c1c7bad5cfa589d9fae350be
devjun10 <devjun1023@gmail.com>
2024-03-27 01:30:11 +0900

    chore: 불필요한 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter:1.19.3")
     testImplementation("org.testcontainers:localstack:1.15.3")
 }
